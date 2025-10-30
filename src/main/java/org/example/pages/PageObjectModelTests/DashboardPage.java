b297bfddefa37742c143d345d39de131dc3a24ce
Anuj Rajput <anujrajput8959@gmail.com>
2025-10-30 23:15:41 +0530

    Wait helper and utilexcel

---
+++ src/main/java/org/example/pages/PageObjectModelTests/DashboardPage.java
@@ -0,0 +1,4 @@
+package org.example.pages.PageObjectModelTests;
+
+public class DashboardPage {
+}
---
+++ src/main/java/org/example/utils/PropertiesReader.java
@@ -0,0 +1,5 @@
+package org.example.utils;
+
+public class PropertiesReader {
+    //File to read te data from the data.properties (key, value)
+}
