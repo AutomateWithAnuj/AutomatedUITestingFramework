f8b449306695f7b68c9615e4bffbc90966b4bbd1
Anuj Rajput <anujrajput8959@gmail.com>
2025-10-31 10:06:25 +0530

    Properties Reader File

--- src/main/java/org/example/utils/PropertiesReader.java
+++ src/main/java/org/example/utils/PropertiesReader.java
@@ -1,5 +1,23 @@
 package org.example.utils;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertiesReader {
     //File to read te data from the data.properties (key, value)
+    public static String readKey(String key){
+        Properties p = null;
+        try {
+            FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"C:\\Users\\DELL\\IdeaProjects\\AutomatedUITestingPrograms_New\\src\\main\\java\\org\\example\\utils\\PropertiesReader.java");
+            p =  new Properties();
+            p.load(fis);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return p.getProperty(key);
+    }
 }
