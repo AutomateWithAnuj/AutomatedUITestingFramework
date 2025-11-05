44804293880ad5aa9671c22c84f6f03bdec6b492
Anuj Rajput <anujrajput8959@gmail.com>
2025-11-05 09:00:22 +0530

    Log4j Usage

---
+++ src/test/java/org/example/listeners/RetryAnalyzer.java
@@ -0,0 +1,17 @@
+package org.example.listeners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+    private int retryCount;
+    private static final int maxRetryCount = 3;
+    @Override
+    public boolean retry(ITestResult result) {
+        if(retryCount<maxRetryCount){
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+}
